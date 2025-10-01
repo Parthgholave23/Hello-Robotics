04cfff327b328cb6c88d2cd3df21acad19f7f416
Parthgholave23 <parthgholave23@gmail.com>
2025-10-01 15:19:22 +0530

    Add files via upload

---
+++ Hello Robotics.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    char name[20];
+    
+    printf("Enter your name:");
+    scanf("%s",&name);
+    
+    printf("Hello Robotics!!\n");
+    
+    printf("Welcome ,%s",&name);
+}
